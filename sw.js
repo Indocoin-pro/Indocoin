8b39b1ad17ef656dc08ce0739a3fde6cd5baa9de
Indocoin-pro <indocoin.info@gmail.com>
2026-06-02 18:06:07 +0800

    Update service worker version to 110.5

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
  * Semua user akan otomatis dapat versi terbaru!
  */
 
-const VERSION = "110.4";
+const VERSION = "110.5";
 const CACHE_NAME = "indocoin-v" + VERSION;
 
 const CACHE_FILES = [
@@ -101,6 +101,7 @@ const CACHE_FILES = [
   "/merchant-pay.html",
   "/merchant-qr.html",
   "/token-lock-tracker.html",
+  "/welcome.html",
   "/premium-games.html",
 ];
 
